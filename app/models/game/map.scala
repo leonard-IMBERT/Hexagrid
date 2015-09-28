0a099d0107dcef1164eaf0addef3ce469ff5e71b
Tigri <leonard.imbert@clever-cloud.com>
2015-09-28 16:31:38 +0200

    Correct indentation and readability of the code

--- app/models/game/map.scala
+++ app/models/game/map.scala
@@ -1,22 +1,22 @@
-  package game
+package game
 
-  import play.api._
+import play.api._
 
-  import scala.math._
-  import scala.math.abs
+import scala.math._
+import scala.math.abs
 
-  object MapManager {
-    case class WorldMap(tileMap: Map[Valid, Charac])
-    case class Charac(terrain: Terrain.TerrainType)
+object MapManager {
+  case class WorldMap(size: Int, tileMap: Map[Valid, Charac])
+  case class Charac(terrain: Terrain.TerrainType)
 
-    type Tile = (Valid, Charac)
+  type Tile = (Valid, Charac)
 
-  val Water = Terrain.Terrain("Water", 5, "#007FFF", 3)
-  val Grass = Terrain.Terrain("Grass", 1, "#00FF7F", 6)
-  val Mountain = Terrain.Terrain("Mountain", 3 , "#603F00", 8)
-  val Rock = Terrain.Terrain("Rock", 4, "#888888", 9)
-  val Snow = Terrain.Terrain("High-Snow", 5, "#EEEEEE", 10)
-  val Sand = Terrain.Terrain("Sand", 1, "#FFFF00", 4)
+  val Water     = Terrain.Terrain("Water",      5, "#007FFF", 3)
+  val Grass     = Terrain.Terrain("Grass",      1, "#00FF7F", 6)
+  val Mountain  = Terrain.Terrain("Mountain",   3, "#603F00", 8)
+  val Rock      = Terrain.Terrain("Rock",       4, "#888888", 9)
+  val Snow      = Terrain.Terrain("High-Snow",  5, "#EEEEEE", 10)
+  val Sand      = Terrain.Terrain("Sand",       1, "#FFFF00", 4)
 
   val terrainListG = List(Water, Grass, Mountain, Sand, Snow, Rock)
 
@@ -29,7 +29,8 @@
     def go(ite: Int, wMap: WorldMap): WorldMap = {
       if(ite < 0) wMap
       else {
-        go(ite - 1, getLayerAround(ite, wMap, center).foldLeft(wMap)((wMapAcc, coor) => setTerrainForTile(terrainListG, wMapAcc, coor)))
+        go(ite - 1, getLayerAround(ite, wMap, center)
+          .foldLeft(wMap)((wMapAcc, coor) => setTerrainForTile(terrainListG, wMapAcc, coor)))
       }
     }
 
@@ -45,8 +46,7 @@
       addTile(wMap, tile, Charac(terrainList.minBy(_.high)), true)
     }
     else {
-      val tmp = addTile(wMap, tile, Charac(determineTerrain(listNeig.map(_._2.terrain), terrainList)), true)
-      tmp
+      addTile(wMap, tile, Charac(determineTerrain(listNeig.map(_._2.terrain), terrainList)), true)
     }
   }
 
@@ -54,10 +54,23 @@
     val redeableTerrain = neigh.map(Terrain.getHigh(_)).collect({case Some(high) => high})
     val moyHigh: Double = math.round(redeableTerrain.sum.toDouble / redeableTerrain.length.toDouble)
 
-    val flatTile: Terrain.Terrain = terrainList.collect({case ter: Terrain.Terrain if (ter.high >= moyHigh) => ter}).sortBy(_.high).headOption.getOrElse(Terrain.Terrain("Nothing", 0 , "#F00", 0))
-  
-    val upperTile: Terrain.Terrain = terrainList.collect({case ter: Terrain.Terrain if(ter.high > moyHigh) => ter}).sortBy(_.high).headOption.getOrElse(flatTile)
-    val downerTile: Terrain.Terrain = terrainList.collect({case ter: Terrain.Terrain if(ter.high < moyHigh) => ter}).sortWith(_.high > _.high).headOption.getOrElse(flatTile)
+    val flatTile: Terrain.Terrain = terrainList
+      .collect({case ter: Terrain.Terrain if (ter.high >= moyHigh) => ter})
+      .sortBy(_.high)
+      .headOption
+      .getOrElse(Terrain.Terrain("Nothing", 0 , "#F00", 0))
+
+    val upperTile: Terrain.Terrain = terrainList
+      .collect({case ter: Terrain.Terrain if(ter.high > moyHigh) => ter})
+      .sortBy(_.high)
+      .headOption
+      .getOrElse(flatTile)
+
+    val downerTile: Terrain.Terrain = terrainList
+      .collect({case ter: Terrain.Terrain if(ter.high < moyHigh) => ter})
+      .sortWith(_.high > _.high)
+      .headOption
+      .getOrElse(flatTile)
 
     val salt = math.random
 
@@ -73,19 +86,20 @@
     val li = for {
       x <- around(layer)(center.x)
       y <- around(layer)(center.y)
-    } yield {
-      newCoordonee(x,y,0-x-y)
-    }
+      } yield {
+        newCoordonee(x,y,0-x-y)
+      }
 
-    li.collect({case co:Valid => co}).filter(co => (abs(co.x) <= center.x + layer && abs(co.y) <= center.y + layer && abs(co.z) <= center.z + layer) && (abs(co.x) == center.x + layer || abs(co.y) == center.y + layer || abs(co.z) == center.z + layer))
+      li.collect({case co:Valid => co})
+        .filter(co => (abs(co.x) <= center.x + layer && abs(co.y) <= center.y + layer && abs(co.z) <= center.z + layer) && (abs(co.x) == center.x + layer || abs(co.y) == center.y + layer || abs(co.z) == center.z + layer))
   }
 
   def genUndefinedMap(size: Int, center: Valid): WorldMap = {
-    getPotentialNeighbourgs(center, size).foldLeft(WorldMap(Map()))((wMap, coor) => addTile(wMap, coor, Charac(Terrain.Undefined), true))
+    getPotentialNeighbourgs(center, size).foldLeft(WorldMap(size, Map()))((wMap, coor) => addTile(wMap, coor, Charac(Terrain.Undefined), true))
   }
 
-  def genGrassMap: WorldMap = {
-    getPotentialNeighbourgs(Valid(0,0,0), 4).foldLeft(WorldMap(Map()))((wMap, coor) => addTile(wMap, coor, Charac(Grass), true))
+  def genGrassMap(size: Int): WorldMap = {
+    getPotentialNeighbourgs(Valid(0,0,0), size).foldLeft(WorldMap(size, Map()))((wMap, coor) => addTile(wMap, coor, Charac(Grass), true))
   }
 
   def newCoordonee(x: Int, y: Int, z: Int): Coordonee = {
@@ -96,11 +110,11 @@
   def addTile(wMap: WorldMap, coor: Valid, char: Charac, over: Boolean): WorldMap = {
     wMap.tileMap.get(coor) match {
       case Some(tile2) => {
-        if(over) WorldMap(wMap.tileMap + ((coor, char)))
-        else if(tile2.terrain == Terrain.Undefined) WorldMap(wMap.tileMap + ((coor, char)))
+        if(over) WorldMap(wMap.size, wMap.tileMap + ((coor, char)))
+        else if(tile2.terrain == Terrain.Undefined) WorldMap(wMap.size, wMap.tileMap + ((coor, char)))
         else wMap
       }
-      case None => WorldMap(wMap.tileMap + ((coor, char)))
+      case None => WorldMap(wMap.size, wMap.tileMap + ((coor, char)))
     }
   }
 
@@ -114,7 +128,7 @@
   }
 
   def getNeighbourgs(wMap: WorldMap, coor: Valid): WorldMap = {
-    getPotentialNeighbourgs(coor, 1).foldLeft(WorldMap(Map()))((map, coor) => {
+    getPotentialNeighbourgs(coor, 1).foldLeft(WorldMap(wMap.size, Map()))((map, coor) => {
       wMap.tileMap.get(coor) match {
         case Some(char) => addTile(map, coor, char, true)
         case None => addTile(map, coor, Charac(Terrain.OutOfBound), false)
@@ -122,13 +136,13 @@
     })
   }
 
-  def removeTile(wMap: WorldMap, coor: Valid): WorldMap = WorldMap(wMap.tileMap - coor)
+  def removeTile(wMap: WorldMap, coor: Valid): WorldMap = WorldMap(wMap.size, wMap.tileMap - coor)
 
   def around(range: Int)(nu: Int): List[Int] = {
     val trueRange = abs(range)  
     val min = nu - trueRange
     val max = nu + trueRange
-    
+
     def go(begin: Int, end: Int): List[Int] = {
       if (begin == end) List(begin)
       else begin :: go(begin + 1, end)
